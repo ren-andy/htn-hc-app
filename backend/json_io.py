d2939d55bd587838d86bab76d4a69b3130a9b8c9
unknown <averyshum62@gmail.com>
2019-09-15 03:30:51 -0400

    Updated flask cors header and api button in App.js

--- backend/json_io.py
+++ backend/json_io.py
@@ -1,8 +1,11 @@
 from flask import Flask, jsonify, request
 import requests
 import os
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 def analyze_text(phrase):
     subscription_key = "b321c4df35534633a4ed9f35ec5e7ae3"
